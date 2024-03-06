4740456903333fc27de7cd96e043fd9dd5ccfb8c
Lucas Saliés Brum <sistematico@gmail.com>
2024-03-06 15:18:25 -0400

    Update

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -29,7 +29,7 @@ export default {
 			},
 			fontFamily: {
 				// Add any custom fonts here
-				sans: [...fontFamily.sans],
+				sans: ['Nunito', ...fontFamily.sans],
 				serif: [...fontFamily.serif],
 			},
 			transitionProperty: {
