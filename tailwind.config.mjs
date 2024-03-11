7f8d235c0398ab02d940b85f272d4ae4bed3d666
Lucas Saliés Brum <sistematico@gmail.com>
2024-03-10 22:53:14 -0400

    Update

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -3,6 +3,9 @@ export default {
 	darkMode: 'selector',
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
+    fontFamily: {
+      'body': ['"Nunito"', ui-sans-serif, system-ui, sans-serif, '"Apple Color Emoji"', '"Segoe UI Emoji"', '"Segoe UI Symbol"', '"Noto Color Emoji"']
+    },
 		extend: {
 			colors: {
 				'paxa-fg': '#F3EEEA',
