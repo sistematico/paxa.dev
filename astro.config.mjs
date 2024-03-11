a1678bab9e33786abb1d95683a9dd961a2f5881a
Lucas Saliés Brum <sistematico@gmail.com>
2024-03-11 12:34:05 -0400

    Re-factor

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,10 @@
 import { defineConfig } from 'astro/config';
+import mdx from '@astrojs/mdx';
+import sitemap from '@astrojs/sitemap';
+import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: 'https://paxa.dev',
+  integrations: [mdx(), sitemap(), tailwind()]
+});
