7ecd4ea1f7dbe30cbe5cde280666de888c3a5746
Lucas Saliés Brum <sistematico@gmail.com>
2024-03-11 16:27:49 -0400

    Master update

---
+++ src/content/blog/novo-site.md
@@ -0,0 +1,19 @@
+---
+title: 'Um novo site!'
+description: ''
+pubDate: 'Mar 08 2016'
+heroImage: '/blog-placeholder-3.jpg'
+tags:
+  - 'novidades'
+---
+
+Olá pessoal!
+
+Alterei meu site pessoal(antigo sistematico.org com o Wordpress) para uma versão hospedada no GitHub Pages usando o Jekyll e a linguagem Ruby.  
+Gostei bastante, sou muito novo com essa linguagem de programação e ainda estou me adaptando.  
+Em breve devo adicionar dicas e alguns snippets neste site, espero que gostem!
+
+Forte Abraço..  
+&#45;&#45;  
+Lucas Saliés Brum    
+a.k.a. sistematico
--- src/content/config.ts
+++ src/content/config.ts
@@ -5,12 +5,26 @@ const blog = defineCollection({
 	// Type-check frontmatter using a schema
 	schema: z.object({
 		title: z.string(),
-		description: z.string(),
+		description: z.string().optional(),
 		// Transform string to Date object
 		pubDate: z.coerce.date(),
 		updatedDate: z.coerce.date().optional(),
 		heroImage: z.string().optional(),
+		tags: z.array(z.string()).optional()
 	}),
 });
+
+// const projectsCollection = defineCollection({
+// 	type: 'data',
+// 	schema: z.object({
+// 		title: z.string(),
+// 		url: z.string()
+// 	}),
+// });
+
+// export const collections = {
+//   'blog': blogCollection,
+//   'projects': projectsCollection
+// };
 
 export const collections = { blog };
