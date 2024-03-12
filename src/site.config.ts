eb9cddf76e09a152d1031abee3eb1a5874b07ade
Lucas Saliés Brum <sistematico@gmail.com>
2024-03-12 09:13:56 -0400

    Cleanup

--- src/site.config.ts
+++
@@ -1,6 +0,0 @@
-export const config = {
-  name: import.meta.env.PUBLIC_APP_NAME ?? 'Paxá',
-  desc: import.meta.env.PUBLIC_APP_DESC ?? 'Desenvolvimento Web',
-  fg: import.meta.env.PUBLIC_APP_FG ?? '#F3EEEA',
-  bg: import.meta.env.PUBLIC_APP_BG ?? '#776B5D'
-}
