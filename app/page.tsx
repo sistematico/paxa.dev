8b1302a93f33f7c0ab22887bf61348f568ccf19d
Lucas Saliés Brum <sistematico@gmail.com>
2024-12-12 22:20:51 -0400

    Update assets

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,13 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <h1>Olá</h1>
+    <Image
+      className="dark:invert"
+      src="/next.svg"
+      alt="Next.js logo"
+      width={180}
+      height={38}
+      priority
+    />
   );
 }
