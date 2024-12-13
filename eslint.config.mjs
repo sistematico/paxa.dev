a33eedef78c71b2c162404ecaf6eb75ed195b575
Lucas Saliés Brum <sistematico@gmail.com>
2024-12-12 23:47:45 -0400

    Updated eslint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -6,11 +6,22 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 const compat = new FlatCompat({
-  baseDirectory: __dirname,
+  // baseDirectory: __dirname,
+  baseDirectory: import.meta.dirname,
 });
 
+// const eslintConfig = [
+//   ...compat.extends("next/core-web-vitals", "next/typescript"),
+// ];
+
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
-];
+  ...compat.config({
+    extends: ["next"],
+    rules: {
+      '@next/next/no-page-custom-font': 'off',
+      '@next/next/prefer-const': 'off'
+    },
+  }),
+]
 
 export default eslintConfig;
