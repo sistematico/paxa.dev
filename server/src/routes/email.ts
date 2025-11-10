df41d3ac27d80b6706dfde60a79daa25e6b53acf
Lucas Saliés Brum <sistematico@gmail.com>
2025-11-10 18:32:42 -0400

    Refactor email route and update formatting settings

--- server/src/routes/email.ts
+++ server/src/routes/email.ts
@@ -1,37 +1,38 @@
-import { Hono } from 'hono';
-import nodemailer from 'nodemailer';
+import { Hono } from "hono"
+import nodemailer from "nodemailer"
 
-const app = new Hono();
+const app = new Hono()
 
-app.post('/', async (c) => {
-	try {
-		const { name, email, subject, message } = await c.req.json();
+app.post("/", async (c) => {
+  try {
+    const { name, email, subject, message } = await c.req.json()
 
-		// Create a Nodemailer transporter
-		const transporter = nodemailer.createTransport({
-			service: "iCloud", 
-			auth: {
-				user: process.env.ICLOUD_EMAIL!, 
-				pass: process.env.ICLOUD_PASSWORD!
-			}
-		});
+    // Create a Nodemailer transporter
+    const transporter = nodemailer.createTransport({
+      service: "iCloud",
+			secure: false,
+      auth: {
+        user: process.env.ICLOUD_EMAIL!,
+        pass: process.env.ICLOUD_PASSWORD!
+      }
+    })
 
-		// Define email options
-		const mailOptions = {
-			from: `"${name}" <${email}>`,
-			to: 'paxa@paxa.dev', // Recipient email address
-			subject: `paxa.dev: ${subject} ${name}`,
-			html: `<p>Nome: ${name}</p><p>E-mail: ${email}</p><p>Mensagem: ${message}</p>`
-		};
+    // Define email options
+    const mailOptions = {
+      from: `"${name}" <${email}>`,
+      to: "paxa@paxa.dev", // Recipient email address
+      subject: `paxa.dev: ${subject} ${name}`,
+      html: `<p>Nome: ${name}</p><p>E-mail: ${email}</p><p>Mensagem: ${message}</p>`
+    }
 
-		// Send the email
-		await transporter.sendMail(mailOptions);
+    // Send the email
+    await transporter.sendMail(mailOptions)
 
-		return c.text('E-mail enviado');
-	} catch (error) {
-		console.error('Erro ao enviar e-mail:', error);
-		return c.text('Erro ao enviar e-mail.', 500);
-	}
-});
+    return c.text("E-mail enviado")
+  } catch (error) {
+    console.error("Erro ao enviar e-mail:", error)
+    return c.text("Erro ao enviar e-mail.", 500)
+  }
+})
 
-export { app as emailRoutes };
+export { app as emailRoutes }
