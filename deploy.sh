ca9141a15c7ce40292e2d49082326ba53ffbb471
Lucas Saliés Brum <sistematico@gmail.com>
2025-11-10 16:37:19 -0400

    Update deployment script to exclude .env.production and add ICLOUD_EMAIL to .env.production

--- deploy.sh
+++ deploy.sh
@@ -13,13 +13,16 @@ cd $TEMPDIR
 
 git clean -fxd \
   -e server/drizzle/database.sqlite \
-  -e client/.env.production \
-  -e server/.env.production
-cp server/.env.production server/.env
-cp client/.env.production client/.env
+  -e client/.env \
+  -e server/.env
+cp server/.env server/.env.production
+cp client/.env client/.env.production
 
 bun install
-cd server && bunx drizzle-kit push && cd ..
+
+cd server 
+bunx drizzle-kit push 
+cd ..
 bun run build:single || { echo "Build failed"; exit 1; }
 
 sudo /usr/bin/systemctl stop $SERVICE
